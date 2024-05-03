188fe1e7cee84bef171871e0e336da0af793a56f
dukc <97273940+dukcc@users.noreply.github.com>
2024-05-03 18:31:34 +0000

    index done

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 export default function Button({text,link,colour}: {text:string, link:string, colour:string}) {
     return (
-        <a href={link} className={`bg-${colour} h-[47px] w-fit px-24 duration-200 ease-out hover:rounded-[24px] text-[24px] flex justify-center items-center border border-transparent hover:border-black`}>{text}</a>
+        <a href={link} className={`bg-${colour} h-[47px] w-fit px-24 duration-200 ease-out hover:rounded-[24px] active:scale-90 text-[24px] flex justify-center items-center border border-transparent hover:border-black whitespace-nowrap`}>{text}</a>
     )
 }
