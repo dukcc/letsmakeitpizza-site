cc1dbecb5fda08a7f5ffd18fe4ae83644080f5ac
dukc <97273940+dukcc@users.noreply.github.com>
2024-05-06 18:32:09 +0000

    about + menu state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,10 @@
 export default function Button({text,link,colour}: {text:string, link:string, colour:string}) {
+    if (colour == "primary") {
+        colour = "#FF5942"
+    } else if (colour == "secondary") {
+        colour = "#FFFFFF"
+    }
     return (
-        <a href={link} className={`bg-${colour} h-[47px] w-fit px-24 duration-200 ease-out hover:rounded-[24px] active:scale-90 text-[24px] flex justify-center items-center border border-transparent hover:border-black whitespace-nowrap`}>{text}</a>
+        <a href={link} style={{backgroundColor: colour}} className={`h-[47px] uppercase w-fit px-24 duration-200 ease-out hover:rounded-[24px] active:scale-90 text-[24px] flex justify-center items-center border border-transparent hover:border-black whitespace-nowrap`}>{text}</a>
     )
 }
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,12 +1,25 @@
-export function Item({title,price,serving,sub}: {title:string, price:string, serving?:string, sub?:boolean}) {
+import { useState } from "react"
+import Button from "./Button"
+
+export function Item({title,price,serving,sub,desc = "Description"}: {title:string, price:string, serving?:string, sub?:boolean, desc:string}) {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleOpen = () => {
+        setIsOpen(!isOpen)
+    }
     return (
-        <div className={`w-full gap-32 items-center text-[32px] flex ${sub ? 
+        <div className={`w-full flex flex-col ${sub ? 
             "pl-64" : ""
         }`}>
-            <h4 className="whitespace-nowrap">{title}</h4>
-            <div className="h-[1px] w-full bg-black/25"></div>
-            <h4 className="text-black/75 whitespace-nowrap">{serving}</h4>
-            <h4 className="text-brand-dark">{price}</h4>
+            <button onClick={toggleOpen} className="flex gap-32 items-center">
+                <h4 className="whitespace-nowrap text-[32px]">{title}</h4>
+                <div className="h-[1px] w-full bg-black/25"></div>
+                <h4 className="text-black/75 whitespace-nowrap  text-[32px]">{serving}</h4>
+                <h4 className="text-brand-dark text-[32px]">{price}</h4>
+            </button>
+            <div className={isOpen ? "flex justify-between" : "hidden"}>
+                <p className="text-[16px]">{desc}</p>
+                <Button colour="primary" text="commande" link="#" />
+            </div>
         </div>
     )
 }
