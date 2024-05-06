169c94d7641fce5edfa1794b6fd620d8de373402
dukc <97273940+dukcc@users.noreply.github.com>
2024-05-06 17:47:02 +0000

    finished menu

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,9 +1,21 @@
-export function Item({title,price}: {title:string, price:string}) {
+export function Item({title,price,serving,sub}: {title:string, price:string, serving?:string, sub?:boolean}) {
+    return (
+        <div className={`w-full gap-32 items-center text-[32px] flex ${sub ? 
+            "pl-64" : ""
+        }`}>
+            <h4 className="whitespace-nowrap">{title}</h4>
+            <div className="h-[1px] w-full bg-black/25"></div>
+            <h4 className="text-black/75 whitespace-nowrap">{serving}</h4>
+            <h4 className="text-brand-dark">{price}</h4>
+        </div>
+    )
+}
+
+export function ItemSection({title}: {title:string}) {
     return (
         <div className="w-full gap-32 items-center text-[32px] flex">
             <h4 className="whitespace-nowrap">{title}</h4>
             <div className="h-[1px] w-full bg-black/25"></div>
-            <h4 className="text-brand-dark">{price}</h4>
         </div>
     )
 }
