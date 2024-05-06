07f359aca05bf831bbeeb43d2a8ae25e06a8e470
dukc <dukkcc@proton.me>
2024-05-06 13:40:05 -0700

    started mobile responsiveness

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,43 +1,55 @@
-import { useState } from "react"
-import Button from "./Button"
+import { useState } from "react";
+import Button from "./Button";
 
-export function Item({title,price,serving,sub,desc = "Description"}: {title:string, price:string, serving?:string, sub?:boolean, desc:string}) {
-    const [isOpen, setIsOpen] = useState(false)
-    const toggleOpen = () => {
-        setIsOpen(!isOpen)
-    }
-    return (
-        <div className={`w-full flex flex-col ${sub ? 
-            "pl-64" : ""
-        }`}>
-            <button onClick={toggleOpen} className="flex gap-32 items-center">
-                <h4 className="whitespace-nowrap text-[32px]">{title}</h4>
-                <div className="h-[1px] w-full bg-black/25"></div>
-                <h4 className="text-black/75 whitespace-nowrap  text-[32px]">{serving}</h4>
-                <h4 className="text-brand-dark text-[32px]">{price}</h4>
-            </button>
-            <div className={isOpen ? "flex justify-between" : "hidden"}>
-                <p className="text-[16px]">{desc}</p>
-                <Button colour="primary" text="commande" link="#" />
-            </div>
-        </div>
-    )
+export function Item({
+  title,
+  price,
+  serving,
+  sub,
+  desc = "Description",
+}: {
+  title: string;
+  price: string;
+  serving?: string;
+  sub?: boolean;
+  desc?: string;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div className={`w-full flex flex-col ${sub ? "pl-64" : ""}`}>
+      <button onClick={toggleOpen} className="flex gap-32 items-center">
+        <h4 className="whitespace-nowrap text-[32px]">{title}</h4>
+        <div className="h-[1px] w-full bg-black/25"></div>
+        <h4 className="text-black/75 whitespace-nowrap  text-[32px]">
+          {serving}
+        </h4>
+        <h4 className="text-brand-dark text-[32px]">{price}</h4>
+      </button>
+      <div className={isOpen ? "flex justify-between" : "hidden"}>
+        <p className="text-[16px]">{desc}</p>
+        <Button colour="primary" text="commande" link="#" />
+      </div>
+    </div>
+  );
 }
 
-export function ItemSection({title}: {title:string}) {
-    return (
-        <div className="w-full gap-32 items-center text-[32px] flex">
-            <h4 className="whitespace-nowrap">{title}</h4>
-            <div className="h-[1px] w-full bg-black/25"></div>
-        </div>
-    )
+export function ItemSection({ title }: { title: string }) {
+  return (
+    <div className="w-full gap-32 items-center text-[32px] flex">
+      <h4 className="whitespace-nowrap">{title}</h4>
+      <div className="h-[1px] w-full bg-black/25"></div>
+    </div>
+  );
 }
 
-export function Section({title,times}: {title:string, times:string}) {
-    return (
-        <div className="w-full justify-between text-[64px] flex">
-            <h4>{title}</h4>
-            <h4>{times}</h4>
-        </div>
-    )
+export function Section({ title, times }: { title: string; times: string }) {
+  return (
+    <div className="w-full justify-between text-[64px] flex">
+      <h4>{title}</h4>
+      <h4>{times}</h4>
+    </div>
+  );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import Button from "./Button";
+import { Bars3Icon, XIcon } from "@heroicons/react/24/outline";
 
 export default function Nav() {
-    return (
-        <nav className="flex sticky top-16 z-50 border border-black h-[47px] overflow-clip">
-            <div className="bg-fg px-24 py-8 w-full flex items-center"><a href="/"><img src="/logo mark.png" alt="Logo" className="size-72" /></a></div>
-            <Button text="MAISON" link="/" colour="primary" />
-            <Button text="COMMANDE" link="/" colour="secondary" />
-            <Button text="MENU" link="/menu" colour="primary" />
-            <Button text="À PROPOS DE" link="/about" colour="secondary" />
-        </nav>
-    )
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className="flex max-[700px]:flex-col max-[700px]:h-fit items-center sticky top-16 z-50 border border-black h-[47px] overflow-clip">
+      <div className="bg-fg px-24 py-8 w-full flex items-center h-[47px] justify-between">
+        <a href="/">
+          <img src="/logo mark.png" alt="Logo" className="size-72" />
+        </a>
+        <button
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+          className="hidden max-[700px]:block size-24"
+        >
+          {isOpen ? <XIcon /> : <Bars3Icon />}
+        </button>
+      </div>
+      <div
+        className={`flex max-[700px]:hidden ${
+          isOpen ? "flex flex-col" : "hidden"
+        }`}
+      >
+        <Button text="MAISON" link="/" colour="primary" />
+        <Button text="COMMANDE" link="/" colour="secondary" />
+        <Button text="MENU" link="/menu" colour="primary" />
+        <Button text="À PROPOS DE" link="/about" colour="secondary" />
+      </div>
+    </nav>
+  );
 }
